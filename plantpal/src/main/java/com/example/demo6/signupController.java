674a44a9703c7930191b9c7c23c87f0b47ef852f
mika-progga <pialeemoni@gmail.com>
2023-11-01 10:13:30 +0600

    forgetpassword,signup

--- plantpal/src/main/java/com/example/demo6/signupController.java
+++ plantpal/src/main/java/com/example/demo6/signupController.java
@@ -5,9 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class signupController {
@@ -32,13 +35,44 @@ public class signupController {
 
     @FXML
     private Button signIn;
+    @FXML
+    private TextField FullnameField;
+    @FXML
+    private TextField Emailfield;
+    @FXML
+    private TextField UsernameField;
+    @FXML
+    private TextField NumberField;
+    @FXML
+    private TextField PasswordField;
+    @FXML
+    private TextField RepeatField;
+
 
     @FXML
     void signInBtn(MouseEvent event) throws IOException {
-        Stage stage = (Stage) signIn.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signsuccess.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
+        String Full=FullnameField.getText();
+        String email=Emailfield.getText();
+        String user=UsernameField.getText();
+        String number=NumberField.getText();
+        String pass=PasswordField.getText();
+        String repeat=RepeatField.getText();
+
+        try(BufferedWriter writer=new BufferedWriter(new FileWriter("src/AllUsers.txt",true)))
+        {
+            String userdata=Full + "," + email +","+ user +","+number+","+pass+","+repeat;
+            writer.write(userdata);
+            writer.newLine();
+            writer.flush();
+            Stage stage = (Stage) signIn.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signsuccess.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
--- plantpal/src/main/java/module-info.java
+++ plantpal/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.demo {
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    requires java.mail;
 
     opens com.example.demo6 to javafx.fxml;
     exports com.example.demo6;
