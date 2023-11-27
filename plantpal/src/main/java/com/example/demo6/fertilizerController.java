660742b945d7feddfbc5e99c75a302bee0bb80fa
zaaaiiimaaa <zaimaahmed101@gmail.com>
2023-11-27 22:00:25 +0600

    fertilizer update 2

--- plantpal/src/main/java/com/example/demo6/fertilizerController.java
+++ plantpal/src/main/java/com/example/demo6/fertilizerController.java
@@ -2,6 +2,7 @@ package com.example.demo6;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
@@ -13,10 +14,52 @@ public class fertilizerController {
     @FXML
     private Button logo;
     @FXML
+    private Button organic;
+    @FXML
     void logoBtn(MouseEvent event) throws IOException {
         Stage stage = (Stage) logo.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
+    }
+    @FXML
+    void npkBtn(MouseEvent event) throws IOException {
+        Stage newstage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fert1.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        newstage.setScene(scene);
+        newstage.setResizable(false);
+        newstage.show();
+    }
+    @FXML
+    void organicBtn(MouseEvent event) throws IOException {
+        Stage newstage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fert2.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        newstage.setScene(scene);
+        newstage.setResizable(false);
+        newstage.show();
+    }
+    @FXML
+    void nitroBtn(MouseEvent event) throws IOException {
+        Stage newstage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fert3.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        newstage.setScene(scene);
+        newstage.setResizable(false);
+        newstage.show();
+    }
+    @FXML
+    void phosphoBtn(MouseEvent event) throws IOException {
+        Stage newstage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fert4.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        newstage.setScene(scene);
+        newstage.setResizable(false);
+        newstage.show();
     }
 }
--- plantpal/src/main/java/com/example/demo6/sellplantController.java
+++ plantpal/src/main/java/com/example/demo6/sellplantController.java
@@ -50,7 +50,7 @@ public class sellplantController {
     @FXML
     private ImageView imageView;
 
-    private String imageDirectoryPath="C:\\Users\\ASUS\\Documents\\GitHub\\PlantPal\\plantpal\\src\\sellimage\\";
+    private String imageDirectoryPath="C:\\GitHub\\PlantPal\\plantpal\\src\\sellimage\\";
 
     static String us;
     public void setuser(String user)
@@ -73,6 +73,7 @@ public class sellplantController {
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
         newstage.setScene(scene);
+        newstage.setResizable(false);
         newstage.show();
     }
     @FXML
@@ -157,7 +158,7 @@ public class sellplantController {
     }
 
     private void saveToTextFile(String te,String text,String desc,String prc, String imagePath, String textFieldContent) throws IOException {
-        try (FileWriter fileWriter = new FileWriter("C:\\Users\\ASUS\\Documents\\GitHub\\PlantPal\\plantpal\\src\\sellimageData.txt", true)) {
+        try (FileWriter fileWriter = new FileWriter("src/sellimageData.txt", true)) {
             fileWriter.write( te+" , "+text+" , "+desc+" , "+prc+" ,"+imagePath  +" , "+ textFieldContent + "\n");
         }
     }
